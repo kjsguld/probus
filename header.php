9f20a97783ed02d84c01c827ee52050bd1af0006
kjsguld <kjsguld@gmail.com>
2016-11-30 18:22:44 +0100

    og meta cleanup

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 $ogImage = 'http://';
 if ( has_post_thumbnail() ) {
-  $ogImage .= the_post_thumbnail_url();
+  $ogImage .= the_post_thumbnail_url('large');
 } else {
   # get defualt
 }
@@ -12,15 +12,15 @@ if ( has_post_thumbnail() ) {
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title('-', true, 'right') . bloginfo('name'); ?></title>
-    <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
+    <title><?php echo wp_title('-', true, 'right') . bloginfo('name'); ?></title>
+    <meta name="description" content="<?php echo bloginfo('description'); ?>">
     <!-- Open Graph properties -->
-    <meta property="og:site_name" content="" />
-    <meta property="og:title" content="<?php wp_title('-', true, 'right') . bloginfo('name'); ?>" />
-    <meta property="og:description" content="<?php echo get_bloginfo('description'); ?>">
+    <meta property="og:site_name" content="<?php echo bloginfo('name'); ?>" />
+    <meta property="og:title" content="<?php wp_title('-', true, 'right'); ?>" />
+    <meta property="og:description" content="<?php echo bloginfo('description'); ?>">
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="<?php echo get_bloginfo('url'); ?>" />
-    <meta property="og:locale" content="<?php echo get_bloginfo('language'); ?>">
+    <meta property="og:url" content="<?php echo bloginfo('url'); ?>" />
+    <meta property="og:locale" content="<?php echo bloginfo('language'); ?>">
     <meta property="og:image" content="<?php echo $ogImage; ?>" />
     <meta property="og:image:width" content="400" />
     <meta property="og:image:height" content="300" />
